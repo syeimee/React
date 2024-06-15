410a5555d8b54b73925cb202e24b1a1006453594
syeimee <yuuguiter1@gmail.com>
2024-06-16 06:37:17 +0900

    props

--- react-guide-material/04_react_basic/src/090_props/start/components/Child.jsx
+++ react-guide-material/04_react_basic/src/090_props/start/components/Child.jsx
@@ -5,10 +5,15 @@
 
 import "./Child.css";
 
-const Child = ({color}) => {
+const Child = ({color,num,fn,bool,obj}) => {
   return (
     <div className= {`component ${color}`}>
       <h3>Hello Component</h3>
+      <h3>{num}</h3>
+      <h3>{fn("Props")}</h3>
+      <h3>{bool ? "true":"false"}</h3>
+      <h3>{obj.name}</h3>
+      <h3>{obj.age}</h3>
     </div>
   );
 };
