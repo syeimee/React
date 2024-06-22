c58875e0fbc38b5fe6e834fe0bd6d0be764e5b3a
syeimee <yuuguiter1@gmail.com>
2024-06-22 19:16:24 +0900

    配列フィルターの実装

--- react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
@@ -2,12 +2,15 @@
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-
+  //animalsの要素をanimalとして受け取る
+  const helloAnimals = animals.map((animal) => <li>hello,{animal}</li>)
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+          {/* {helloAnimals} */}
+          {/* for文はjsxに記述できないが、mapならできるので、Reactではしばしばこちらを使用する */}
+          {animals.map((animal) => <li key = {animal}>hello,{animal}</li>)}
       </ul>
     </>
   );
--- react-guide-material/06_control_and_form/src/040_list_and_filter/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/040_list_and_filter/start/Example.jsx
@@ -3,13 +3,18 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [filterVal,setFilterVal] = useState("");
   return (
     <>
       <h3>配列のフィルター</h3>
       <ul>
+        <input type="text" value={filterVal} onChange={(e)=>
+        setFilterVal(e.target.value)}/>
         {animals
+          // 不一致:-1
+          .filter(animal => animal.indexOf(filterVal)!= -1)
           .map((animal) => (
-          <li>{animal}</li>
+          <li key={animal}>{animal}</li>
         ))}
       </ul>
     </>
