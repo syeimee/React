9b871af377623df28c38b5421c39c85be2281edf
syeimee <yuuguiter1@gmail.com>
2024-06-22 11:56:59 +0900

    三項演算子によって表示されるコンポーネントを識別する

--- react-guide-material/05_state_and_event/src/064_state_array/start/Example.jsx
+++ react-guide-material/05_state_and_event/src/064_state_array/start/Example.jsx
@@ -1,9 +1,18 @@
+import {useState} from "react"
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
-
+  const [nums, setNums] = useState(numArray);
+  const shuffle = ()=>{
+    const newNums = [...nums];
+      const popValue = newNums.pop();
+      newNums.unshift(popValue);  
+      setNums(newNums);
+  }
   return (
     <>
-      
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
